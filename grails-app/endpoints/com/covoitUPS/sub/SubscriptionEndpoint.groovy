3f036b4ef3042977a81369db3af4b49204af7634
Knarf64 <franck.arrecot@gmail.com>
2013-03-30 15:13:40 +0100

    Endpoint with shitty namespace to change

---
+++ grails-app/endpoints/com/covoitUPS/sub/SubscriptionEndpoint.groovy
@@ -0,0 +1,14 @@
+package com.covoitUPS.sub
+
+
+
+class SubscriptionEndpoint {
+	
+	// TODO : namespace to change
+	def static namespace = "http://yourcompany.com/schemas" 
+	
+	def invoke = { request, response ->
+        // TODO Deal with the incoming XML document here.
+        null
+    }
+}
