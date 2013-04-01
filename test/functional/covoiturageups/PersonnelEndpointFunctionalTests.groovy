a0d638f12d7819e30a63c748647c89f86bdae2ed
Knarf64 <franck.arrecot@gmail.com>
2013-04-01 21:08:14 +0200

    ENdpoint retabli

--- test/functional/covoiturageups/PersonnelEndpointFunctionalTests.groovy
+++ test/functional/covoiturageups/PersonnelEndpointFunctionalTests.groovy
@@ -1,13 +1,9 @@
 package covoiturageups
-
-
-
 import org.codehaus.groovy.grails.plugins.spring.ws.EndpointFunctionalTestCase
 
 class PersonnelEndpointFunctionalTests extends EndpointFunctionalTestCase {
 
-//	def serviceURL = "http://localhost:8080/covoiturageUPS/services/v1/Subscribtion"
-	def serviceURL = "http://localhost:8080/covoiturageUPS/services"
+	def serviceURL = "http://localhost:8080/covoiturageUPS/services/v1/Subscribtion"
 	def namespace = "http://www.covoiturageups.com/v1/definitions"
 
 	void setUp(){
@@ -16,6 +12,43 @@ class PersonnelEndpointFunctionalTests extends EndpointFunctionalTestCase {
 	}
 
 	void testSOAPDocumentService() {
+		// OK Test
+		def response = withEndpointRequest(serviceURL) {
+			SubscriptionRequest(xmlns: namespace) {
+				Subscription{
+					Prenom("Toto")
+					Nom("Auzoo")
+					Mail("totoauzoo@univ-tlse3.fr")
+					Adresse("11 rue l'Homme armee, Toulouse")
+				}
+			}
+		}
+		
+		/*
+		// KO : test failed 
+			// code 100 : mail already used
+		def response = withEndpointRequest(serviceURL) {
+			SubscriptionRequest(xmlns: namespace) {
+				Subscription{
+					Prenom("Toto")
+					Nom("Auzoo")
+					Mail("totoauzoo@univ-tlse3.fr")
+					Adresse("11 rue l'Homme armee, Toulouse")
+				}
+			}
+		}
+			// code 110 : Invalid Mail
+		def response = withEndpointRequest(serviceURL) {
+			SubscriptionRequest(xmlns: namespace) {
+				Subscription{
+					Prenom("Toto")
+					Nom("Auzoo")
+					Mail("fail-univ-tlse3.fr")
+					Adresse("11 rue l'Homme armee, Toulouse")
+				}
+			}
+		}
+			// code 200 : Adresse unknown by OSM
 		def response = withEndpointRequest(serviceURL) {
 			SubscriptionRequest(xmlns: namespace) {
 				Subscription{
@@ -24,10 +57,10 @@ class PersonnelEndpointFunctionalTests extends EndpointFunctionalTestCase {
 					Mail("totoauzoo@univ-tlse3.fr")
 					Adresse("11 rue bernard mule")
 				}
-				
 			}
 		}
-
+		*/
+		
 		def status = response.status
 		assert status == "complete"
 	}
