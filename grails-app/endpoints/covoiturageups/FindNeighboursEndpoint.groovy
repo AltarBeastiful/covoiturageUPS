606b972dec0b7bf9ba615a7b273c717ec2d9e159
legaliz_me <r3m1.benoit@gmail.com>
2013-04-02 17:20:32 +0200

    Implement findNeighbour Endpoint

--- grails-app/endpoints/covoiturageups/FindNeighboursEndpoint.groovy
+++ grails-app/endpoints/covoiturageups/FindNeighboursEndpoint.groovy
@@ -1,20 +1,59 @@
 package covoiturageups
 
+import javax.sound.midi.ControllerEventListener;
+import org.jcouchdb.exception.NotFoundException
+import covoiturageups.Personnel
+
+import external.CalculateDistance;
+
+@Grab(group='com.google.code.jcouchdb', module='jcouchdb', version='1.0.1-1')
 
 
 class FindNeighboursEndpoint {
 	
 	def static namespace = "http://www.covoiturageups.com/v1/definitions"
 		
-	def invoke = { request, response ->
-		// Using the incoming document
-		println "Neighbour Request Received!"
-		println "Mail: ${request.FindNeighbours.Mail[0].text()}"
-		println "Rayon: ${request.FindNeighbours.Rayon[0].text()}"
+	def invoke = { request, response ->		
+		def controller = new PersonnelController()
+		Personnel p1
 		
+		try{
+			p1 = controller.get(request.FindNeighbours.Mail[0].text())
+		}catch(NotFoundException){
+			println "Caller not found"
+			response.FindNeighboursResponse(xmlns: namespace) {
+				status('120')
+				message('Caller could not be found')
+			}
+			return
+		}		
+		double rayon = request.FindNeighbours.Rayon[0].text().toDouble()
+		
+		List<Personnel> friendlyNeighbours = []
+		
+		for(Personnel p2 : controller.list(0)){
+			if(p1.getEmail() != p2.getEmail()){
+				def dist = CalculateDistance.distFrom(p1.getLatitude(), p1.getLongitude(), p2.getLatitude(), p2.getLongitude())
+				if(dist <= rayon){
+					friendlyNeighbours.add(p2)
+				}
+			}
+		}
+		
+		//println "Neighbours : " + friendlyNeighbours
 		
 		response.FindNeighboursResponse(xmlns: namespace) {
-		  status('complete')
+			status('0')
+			message('Found '+ friendlyNeighbours.size() + ' neighbours')
+			
+			for (Personnel nb : friendlyNeighbours) {
+				Neighbour {
+					Prenom(nb.getPrenom())
+					Nom(nb.getNom())
+					Mail(nb.getEmail())
+					Adresse(nb.getAdresse())
+				}
+			}
 		}
     }
 }
--- test/functional/covoiturageups/FindNeighboursEndpointFunctionalTests.groovy
+++ test/functional/covoiturageups/FindNeighboursEndpointFunctionalTests.groovy
@@ -1,19 +1,84 @@
 package covoiturageups
 
 import org.codehaus.groovy.grails.plugins.spring.ws.EndpointFunctionalTestCase
+import org.springframework.web.context.request.RequestContextHolder
+import org.junit.Before;
+import org.junit.Test;
 
 class FindNeighboursEndpointFunctionalTests extends EndpointFunctionalTestCase {
 	
 	def serviceURL = "http://localhost:8080/covoiturageUPS/services/v1/FindNeighbours"
 	def namespace = "http://www.covoiturageups.com/v1/definitions"
+	def controller
 
     void setUp(){
       super.setUp()
       webServiceTemplate.setDefaultUri(serviceURL)
+	  
     }
+	
+	// Execute test in the order we want
+	void testPersonnelEndpoint(){
+		setupDB()
+		
+		userNotExists()
+		populateCaller()
+		noNeighbours()
+		populateNeighbours()
+		allNeighbours()
+		someNeighbours()
+		noNeighboursInRadius()
+	}
+	
+	void setupDB() {
+		controller = new PersonnelController()
+		
+		println "Calling setup"
+		PersonnelService.getInstance().setDatabaseName("covoiturageupstest")
+		PersonnelService.getInstance().flushDatabase()
+	}
+	
+	void populateCaller(){
+		Map myParams = [nom: 'Auzoo2',
+						prenom: 'TotoPasBien',
+						email: 'totoauzoo@univ-tlse3.fr',
+						adresse: 'rue Bayard Toulouse',
+						latitude: 43.5936951,
+						longitude: 1.4380074]
+		
+		assertTrue PersonnelService.getInstance().savePersonnel(new Personnel(myParams))
+	}
+	
+	void populateNeighbours() {
+		Map myParams = [nom: 'Arrecot',
+						prenom: 'franc',
+						email: 'franck@univ-tlse3.fr',
+						adresse: 'place du capitole toulouse',
+						latitude: 43.6043956,
+						longitude: 1.44335225594076]
+		
+		assertTrue PersonnelService.getInstance().savePersonnel(new Personnel(myParams))
+		
+		myParams = [nom: 'Devathaire',
+					prenom: 'Paolo',
+					email: 'paolo@univ-tlse3.fr',
+					adresse: 'rue de la victoire tarbes',
+					latitude: 43.2343555,
+					longitude: 0.0706346]
+		
+		assertTrue PersonnelService.getInstance().savePersonnel(new Personnel(myParams))
+		
+		myParams = [nom: 'Benoit',
+					prenom: 'Remi',
+					email: 'remi@univ-tlse3.fr',
+					adresse: 'Rue bernard mule Toulouse',
+					latitude: 43.5974145,
+					longitude: 1.4575596]
+		
+		assertTrue PersonnelService.getInstance().savePersonnel(new Personnel(myParams))		
+	}
     
-	void testSOAPDocumentService() {
-		// OK Test
+	void userNotExists() {
 		def response = withEndpointRequest(serviceURL) {
 			FindNeighboursRequest(xmlns: namespace) {
 				FindNeighbours{
@@ -23,8 +88,71 @@ class FindNeighboursEndpointFunctionalTests extends EndpointFunctionalTestCase {
 			}
 		}
 		
+		assert response.status == "120"
+		assert response.message == "Caller could not be found"
+	}
+	
+	void noNeighbours() {
+		def response = withEndpointRequest(serviceURL) {
+			FindNeighboursRequest(xmlns: namespace) {
+				FindNeighbours{
+					Mail("totoauzoo@univ-tlse3.fr")
+					Rayon(500)
+				}
+			}
+		}
 		
-		def status = response.status
-		assert status == "complete"
+		assert response.status == "0"
+		assert response.Neighbour.size() == 0
+	}
+	
+	void allNeighbours() {
+		def response = withEndpointRequest(serviceURL) {
+			FindNeighboursRequest(xmlns: namespace) {
+				FindNeighbours{
+					Mail("totoauzoo@univ-tlse3.fr")
+					Rayon(500)
+				}
+			}
+		}
+		
+		assert response.status == "0"
+		assert response.Neighbour.size() == 3
+		
+		//TODO check if the neighbour returned are the correct one (paolo, franck, remi)
+//		for(int i = 0; i < response.Neighbour.size(); i++){
+//			println response.Neighbour[i].Prenom
+//			println response.Neighbour[i].Mail
+//		}
+	}
+	
+	void someNeighbours() {
+		def response = withEndpointRequest(serviceURL) {
+			FindNeighboursRequest(xmlns: namespace) {
+				FindNeighbours{
+					Mail("totoauzoo@univ-tlse3.fr")
+					Rayon(50)
+				}
+			}
+		}
+		
+		assert response.status == "0"
+		assert response.Neighbour.size() == 2
+		
+		//TODO check if the neighbour returned are the correct one (franck, remi)
+	}
+	
+	void noNeighboursInRadius() {
+		def response = withEndpointRequest(serviceURL) {
+			FindNeighboursRequest(xmlns: namespace) {
+				FindNeighbours{
+					Mail("totoauzoo@univ-tlse3.fr")
+					Rayon(1)
+				}
+			}
+		}
+		
+		assert response.status == "0"		
+		assert response.Neighbour.size() == 0
 	}
 }
